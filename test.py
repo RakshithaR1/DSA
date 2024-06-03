e30ce6c07e97e2afc033a5e13883ad078d974373
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2024-06-03 17:31:49 +0530

    test

--- test.py
+++ test.py
@@ -4,4 +4,15 @@
 #     self.b=int(input("Enter the second number:"))
 # a=input_no()
 # print(a.a,a.b)
-c=str(1+2)
+# c=str(1+2)
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# Creating an instance of Person
+p = Person("Alice", 30)
+
+# Printing the instance
+print(p)  # Output: <__main__.Person object at 0x7f9d9d6f4a90>
+
