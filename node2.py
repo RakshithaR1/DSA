a894cf0e5aa073163e304eb7a4701bb42a88a2ed
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2024-06-03 16:53:17 +0530

    test

---
+++ node2.py
@@ -0,0 +1,26 @@
+class Node:
+  def __init__(self,value):
+    self.data=value
+    self.next=None
+  def add_node(self,a,value):
+    current=Node(value)
+    head =a
+    if head.next is None:
+      head.next=current
+    else:
+      c=a
+      while c.next is not None:
+        c=c.next
+      c.next=current
+      
+      
+a=Node("srini")
+a.add_node(a,"hari")
+a.add_node(a,'trert')
+current = a
+while current:
+  print(current.data)
+  current=current.next
+      
+    
+    
