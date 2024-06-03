2346129876c39527e6314c291d94417a7f045ef6
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2024-06-03 16:59:50 +0530

    test

---
+++ classes.py
@@ -0,0 +1,8 @@
+class input_no:
+  def __init__(self):
+    self.a=int(input("Enter the first number: "))
+    self.b=int(input("Enter the second number:"))
+
+class add:
+  def __init__():
+    
---
+++ test.py
@@ -0,0 +1,7 @@
+class input_no:
+  def __init__(self):
+    self.a=int(input("Enter the first number: "))
+    self.b=int(input("Enter the second number:"))
+a=input_no()
+a
+print(a.a,a.b)
